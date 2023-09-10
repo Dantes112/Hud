be7ec1106459d3530168306a93610c63935d2009
Dantes112 <103823028+Dantes112@users.noreply.github.com>
2023-09-10 16:09:34 +0300

    Добавьте файлы проекта.

---
+++ Config.cs
@@ -0,0 +1,66 @@
+﻿using Rocket.API;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using UnityEngine;
+
+
+namespace NameSpaceID 
+{
+    public class HudConfig : IRocketPluginConfiguration
+    {
+        public ushort EffectID;
+        public byte EffectLayer;
+        public string HealthIcon;
+        public string OxygenIcon;
+        public string FoodIcon;
+        public string VirusIcon;
+        public string EnergyIcon;
+        public string WaterIcon;
+
+
+        public bool ShowHealth;
+        public bool ShowOxygen;
+        public bool ShowFood;
+        public bool ShowVirus;
+        public bool ShowEnergy;
+        public bool ShowWater;
+
+
+        public bool CustomHealth;
+        public bool CustomOxygen;
+        public bool CustomFood;
+        public bool CustomVirus;
+        public bool CustomEnergy;
+        public bool CustomWater;
+
+        public void LoadDefaults()
+        {
+            EffectID = 15555;
+            EffectLayer = 55;
+            HealthIcon = "YourUrlHere";
+            WaterIcon = "YourUrlHere";
+            VirusIcon = "YourUrlHere";
+            EnergyIcon = "YourUrlHere";
+            OxygenIcon = "YourUrlHere";
+
+            ShowHealth = true;
+            ShowOxygen = true;
+            ShowFood = true;
+            ShowVirus = true;
+            ShowEnergy = true;
+            ShowWater = true;
+
+            CustomEnergy = false;
+            CustomWater = false;
+            CustomHealth = false;
+            CustomFood = false;
+            CustomOxygen = false;
+            CustomVirus = false;
+        }
+    }
+}
+ 
---
+++ Hud.cs
@@ -0,0 +1,109 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks;
+using NameSpaceID;
+
+namespace Plugin_1
+{
+    internal class CustomHudWendi : RocketPlugin<HudConfig>
+    {
+        public static CustomHudWendi Instance;
+        private HudConfig cfg;
+
+
+        protected override void Load()
+        {
+            Instance = this;
+            cfg = CustomHudWendi.Instance.Configuration.Instance;
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+        }
+
+        protected override void Unload()
+        {
+
+
+        }
+
+        private void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+            SetWidgetFlags(player);
+            ShowHud(player);
+        }
+
+        private void ShowHud(UnturnedPlayer player)
+        {
+            EffectManager.sendUIEffect(cfg.EffectID, cfg.EffectLayer, player.CSteamID, true);
+            if (cfg.ShowEnergy)
+            {
+                SendVisible("Energy", player, true);
+            }
+            else
+            {
+                SendVisible("Energy", player, false);
+            }
+            if (cfg.ShowHealth)
+            {
+                SendVisible("Health", player, true);
+            }
+            else
+            {
+                SendVisible("Health", player, false);
+            }
+            if (cfg.ShowFood)
+            {
+                SendVisible("Food", player, true);
+            }
+            else
+            {
+                SendVisible("Food", player, false);
+            }
+            if (cfg.ShowOxygen)
+            {
+                SendVisible("Oxygen", player, true);
+            }
+            else
+            {
+                SendVisible("Oxygen", player, false);
+            }
+            if (cfg.ShowVirus)
+            {
+                SendVisible("Virus", player, true);
+            }
+            else
+            {
+                SendVisible("Virus", player, false);
+            }
+            if (cfg.ShowWater)
+            {
+                SendVisible("Water", player, true);
+            }
+            else
+            {
+                SendVisible("Water", player, false);
+            }
+
+        }
+        private void SendVisible(string Name, UnturnedPlayer player, bool visible)
+        {
+            EffectManager.sendUIEffectVisibility(cfg.EffectLayer, player.CSteamID, true, Name+"Bar", visible);
+        }
+        private void SetWidgetFlags(UnturnedPlayer player)
+        {
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowFood, false);
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowWater, false);
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowVirus, false);
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowStamina, false);
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowOxygen, false);
+            player.Player.setPluginWidgetFlag(EPluginWidgetFlags.ShowHealth, false);
+        }
+        
+    }
+}
